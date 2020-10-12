fb2e38c6b76f2aece0059a53050fd73ed78c4c9e
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-12 13:51:49 -0400

    delete route and code is now functional

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,6 @@
 // Dependencies
 const router = require('express').Router();
-const { postNote, validateNote, findById } = require('../../lib/notes');
+const { postNote, validateNote, findById, deleteByID } = require('../../lib/notes');
 const { notes } = require('../../db/db');
 
 // Routes used for the Notes API
@@ -25,16 +25,29 @@ router.get('/notes/:id', (req, res) => {
 router.post('/notes', (req, res) => {
     // set the id for the new note based on what the next index of the array will be
     req.body.id = notes.length.toString();
-    // validate the new animal data, and if anything in the req.body is incorrect, send back a 400 error "Bad Request"
+    // validate the new note data, and if anything in the req.body is incorrect, send back a 400 error "Bad Request"
     if(!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
     } else {
-        // add note to json file and animals array with createNewAnimal()
+        // add note to json file and notes array with createNewNote()
         const note = postNote(req.body, notes);
-        // send the response (new animal) in json format back to the client
+        // send the response (new note) in json format back to the client
         res.json(note);
     }
 });
+
+// A route for deleting a note
+router.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const noteExists = findById(noteId, notes);
+    if (noteExists) {
+      const newNotes = deleteByID(noteId, notes);
+      res.json(newNotes);
+    } 
+    else {
+      res.send(404);
+    }
+})
 
 // Export the router - making sure there is only one server instance as instantiated in server.js
 module.exports = router;
